b8ec40f7810cde3b094e3fd07109cfc24c4aefd7
Jeff Haynie <jhaynie@agentuity.com>
2025-12-08 16:28:06 -0600

    chore(sound): Select a default sound (#370)

--- packages/cli/src/sound.ts
+++ packages/cli/src/sound.ts
@@ -1,26 +1,10 @@
-import { join } from 'node:path';
-
 export function playSound(): void {
 	const platform = process.platform;
 
 	let command: string[];
 	switch (platform) {
 		case 'darwin': {
-			const items = [
-				'Blow.aiff',
-				'Bottle.aiff',
-				'Frog.aiff',
-				'Funk.aiff',
-				'Glass.aiff',
-				'Hero.aiff',
-				'Morse.aiff',
-				'Ping.aiff',
-				'Pop.aiff',
-				'Purr.aiff',
-				'Sosumi.aiff',
-			] as const;
-			const file = items[Math.floor(Math.random() * items.length)];
-			command = ['afplay', join('/System/Library/Sounds', file)];
+			command = ['afplay', '/System/Library/Sounds/Blow.aiff'];
 			break;
 		}
 		case 'linux':
