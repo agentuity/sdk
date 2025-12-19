6d33f8e3c31ae23571666f855e3fd5a0b59865e6
Jeff Haynie <jhaynie@gmail.com>
2025-12-19 16:13:56 -0600

    more fixes

--- packages/cli/src/cmd/build/entry-generator.ts
+++ packages/cli/src/cmd/build/entry-generator.ts
@@ -83,7 +83,7 @@ export async function generateEntryFile(options: GenerateEntryOptions): Promise<
 	].filter(Boolean);
 
 	imports.push(`import { type LogLevel } from '@agentuity/core';`);
-	imports.push(`import { bootstrapRuntimeEnv } from '@agentuity/server';`);
+	imports.push(`import { bootstrapRuntimeEnv } from '@agentuity/runtime';`);
 
 	// Generate route mounting code for all discovered routes
 	const routeImportsAndMounts: string[] = [];
