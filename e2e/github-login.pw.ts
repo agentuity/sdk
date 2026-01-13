1a58523b0c3115d7e59b206e6e52ccd3459bf811
Bobby Christopher <b.christopher.3rd@gmail.com>
2026-01-13 11:28:13 -0500

    fix: use /bin/bash shell for execSync in github-login tests

--- e2e/github-login.pw.ts
+++ e2e/github-login.pw.ts
@@ -55,6 +55,7 @@ function runCli(args: string): string {
 			cwd: TEST_PROJECT_DIR,
 			timeout: 60000,
 			stdio: ['pipe', 'pipe', 'pipe'],
+			shell: '/bin/bash',
 		});
 		console.log('Output:', result);
 		return result;
