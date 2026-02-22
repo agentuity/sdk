5a0a809263853562c4633cdbc5a44899d30769c2
Jeff Haynie <jhaynie@gmail.com>
2026-02-21 21:35:18 -0600

    fix(vscode): remove duplicate getStatusColor method in sandboxTreeData

--- packages/vscode/src/features/sandboxExplorer/sandboxTreeData.ts
+++ packages/vscode/src/features/sandboxExplorer/sandboxTreeData.ts
@@ -150,25 +150,6 @@ export class SandboxTreeItem extends vscode.TreeItem {
 		}
 	}
 
-	private getStatusColor(status: SandboxStatus): vscode.ThemeColor | undefined {
-		switch (status) {
-			case 'idle':
-				return new vscode.ThemeColor('charts.blue');
-			case 'running':
-				return new vscode.ThemeColor('charts.green');
-			case 'paused':
-			case 'stopping':
-			case 'suspended':
-				return new vscode.ThemeColor('charts.yellow');
-			case 'failed':
-				return new vscode.ThemeColor('charts.red');
-			case 'terminated':
-				return new vscode.ThemeColor('disabledForeground');
-			default:
-				return undefined;
-		}
-	}
-
 	private formatSandboxTooltip(): string {
 		if (!this.sandboxData) return '';
 
