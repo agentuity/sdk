5be0311136ae7906d1d1a6bb5b2c716ac4c2ccff
huijiro <huijirohankei@gmail.com>
2026-03-29 13:18:11 -0300

    fix(template): fixed exportst on app.ts

--- templates/_base/app.ts
+++ templates/_base/app.ts
@@ -1,8 +1,12 @@
 import { createApp } from '@agentuity/runtime';
 import api from './src/api/index';
 
-const { server, logger } = await createApp({
+const app = await createApp({
 	router: { path: '/api', router: api },
 });
 
+const {logger, server} = app;
+
 logger.debug('Running %s', server.url);
+
+export default app;
